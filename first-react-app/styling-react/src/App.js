e1281cc8378c6853144e4fd3515075a80ca46859
galaxy4276 <galaxyhi4276@gmail.com>
2020-08-15 19:32:01 +0900

    styled-components 학습

--- first-react-app/styling-react/src/App.js
+++ first-react-app/styling-react/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import SassComponent from './Components/SassComponent';
+import CSSModule from './Components/CSSModule';
+import StyledComponent from './Components/StyledComponent';
 
 
 function App() {
   return (
     <div>
-      <SassComponent />
+      <StyledComponent />
     </div>
   );
 }
