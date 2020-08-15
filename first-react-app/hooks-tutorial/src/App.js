050193fe933e25376d65ad6444a82abb872cef78
galaxy4276 <galaxyhi4276@gmail.com>
2020-08-15 16:55:37 +0900

    Hooks ㄱ 끝

--- first-react-app/hooks-tutorial/src/App.js
+++ first-react-app/hooks-tutorial/src/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Counter from './Components/Counter';
 import Info from './Components/Info';
+import Average from './Components/Average';
 
 
 function App() {
   return (
-    <Info />
+    <div>
+      <Average />
+    </div>
   );
 }
 
 export default App;
+
+
+
+/* <button
+  onClick={() => {
+    setVisible(!visible);
+    (() => console.log(visible))();
+  }}
+  >
+    {visible ? '숨기기' : '보이기'}
+  </button>
+  <hr />
+  {visible && <Info />} */
--- first-react-app/hooks-tutorial/src/Components/Info.js
+++ first-react-app/hooks-tutorial/src/Components/Info.js
@@ -1,31 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
+import useInputs from './useInputs';
+
+function reducer(state, action) {
+  console.log(action);
+  return {
+    ...state,
+    [action.name]: action.value,
+  };
+}
 
 const Info = () => {
-  const [name, setName] = useState('');
-  const [nickname, setNickname] = useState('');
+  const [state, dispatch] = useReducer(reducer, {
+    name: '',
+    nickname: '',
+  });
 
-  const onChangeName = (e) => {
-    setName(e.target.value);
-  };
-
-  const onChangeNickname = (e) => {
-    setNickname(e.target.value);
-  };
-
-  const styles = {
-    padding: "5px 10px",
-    marginBottom: "10px",
-  };
+  const { name, nickname } = state;
+  
+  const onChange = (e) => dispatch(e.target);
 
   return (
     <div>
       <div>
-        <input onChange={ onChangeName } placeholder="이름을 입력하세요."  style={ styles } /><br />
-        <input onChange={ onChangeNickname } placeholder="닉네임을 입력하세요." style={ styles } /><br />
+        <input name="name" value={name} onChange={onChange} />
+        <input name="nickname" value={nickname} onChange={onChange} />
       </div>
       <div>
-        <p><b>이름: </b>{name}</p>
-        <p><b>닉네임: </b>{nickname}</p>
+        <div>
+          <b>이름: </b> {name}
+        </div>
+        <div>
+          <b>닉네임: </b> {nickname}
+        </div>
       </div>
     </div>
   );
@@ -33,3 +39,68 @@ const Info = () => {
 
 
 export default Info;
+
+
+
+
+
+
+
+
+
+
+// const Info = () => {
+//   const [name, setName] = useState('');
+//   const [nickname, setNickname] = useState('');
+//   // useEffect(() => {
+//   //   console.log('렌더링이 완료되었습니다!');
+//   //   console.log({
+//   //     name,
+//   //     nickname
+//   //   });
+//   // });
+
+//   // useEffect(() => console.log("마운트될 때 수행됩니다."), []);
+
+//   // useEffect(() => {
+//   //   console.log(name);
+//   // }, [name]);
+
+//   useEffect(() => {
+//     console.log('Effect');
+//     console.log(name);
+//     return () => { // cleanup?
+//       console.log('cleanup');
+//       console.log(name);
+//     };
+//   });
+
+//   const onChangeName = (e) => {
+//     setName(e.target.value);
+//   };
+
+//   const onChangeNickname = (e) => {
+//     setNickname(e.target.value);
+//   };
+
+//   const styles = {
+//     padding: "5px 10px",
+//     marginBottom: "10px",
+//   };
+
+//   return (
+//     <div>
+//       <div>
+//         <input onChange={ onChangeName } placeholder="이름을 입력하세요."  style={ styles } /><br />
+//         <input onChange={ onChangeNickname } placeholder="닉네임을 입력하세요." style={ styles } /><br />
+//       </div>
+//       <div>
+//         <p><b>이름: </b>{ name }</p>
+//         <p><b>닉네임: </b>{nickname}</p>
+//       </div>
+//     </div>
+//   );
+// };
+
+
+// export default Info;
