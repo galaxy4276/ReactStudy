6a0dfeff7858db21f3c9b4e7cf856890532ee9fe
galaxy4276 <galaxyhi4276@gmail.com>
2020-08-25 17:56:20 +0900

    redux 개념 도입

---
+++ first-react-app/vanila-redux/index.js
@@ -0,0 +1,70 @@
+import { createStore } from 'redux';
+
+
+const divToggle = document.querySelector('.toggle');
+const counter = document.querySelector('h1');
+const btnIncrease = document.querySelector('#increase');
+const btnDecrease = document.querySelector('#decrease');
+
+
+// action
+const TOGGLE_SWITCH = 'TOGGLE_SWITCH';
+const INCREASE = 'INCREASE';
+const DECREASE = 'DECREASE';
+
+// Action Function
+const toggleSwitch = () => ({ type: TOGGLE_SWITCH });
+const increase = (difference) => ({ type: INCREASE, difference });
+const decrease = () => ({ type: DECREASE });
+
+const initialState = {
+  toggle: false,
+  counter: 0
+};
+
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case TOGGLE_SWITCH:
+      return {
+        ...state,
+        toggle: !state.toggle
+      };
+    case INCREASE:
+      return {
+        ...state,
+        counter: state.counter + action.difference
+      };
+    case DECREASE:
+      return {
+        ...state,
+        counter: state.counter - 1
+      };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+const render = () => {
+  const state = store.getState();
+
+  if (state.toggle) {
+    divToggle.classList.add('active');
+  } else {
+    divToggle.classList.remove('active');
+  }
+
+  counter.innerText = state.counter;
+}
+
+render();
+
+const listener = () => {
+  console.log('상태가 업데이트됨');
+}
+
+const unsubscribe = store.subscribe(listener);
+
+unsubscribe();
